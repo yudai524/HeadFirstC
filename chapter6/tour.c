35070620161d98f85bc6402345d4c6b99ed2ffbf
yudaikonishi <hogehogehoihoi@gmail.com>
2016-11-05 17:21:16 +0900

    practice of free()

--- chapter6/tour.c
+++ chapter6/tour.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct island {
   char *name;
@@ -17,20 +19,44 @@ void display(island *start)
   }
 }
 
+island* create(char *name)
+{
+  island *i = malloc(sizeof(island));
+
+  i->name = strdup(name);
+  i->opens = "09:00";
+  i->closes = "17:00";
+  i->next = NULL;
+
+  return i;
+}
+
+void release(island *start)
+{
+  island *i = start;
+  island *next = NULL;
+  for (; i != NULL; i = next) {
+    next = i->next;
+    free(i->name);
+    free(i);
+  }
+}
+
 int main() {
-  island amity = {"アミティ", "09:00", "17:00", NULL};
-  island craggy = {"クラっギー", "09:00", "17:00", NULL};
-  island isla_nublar = {"イスラぬブラる", "09:00", "17:00", NULL};
-  island shutter = {"シャッター", "09:00", "17:00", NULL};
 
-  amity.next = &craggy;
-  craggy.next = &isla_nublar;
-  isla_nublar.next = &shutter;
+  island *start = NULL;
+  island *i = NULL;
+  island *next = NULL;
+  char name[80];
 
-  island skull = {"スカル", "09:00", "17:00", NULL};
+  for(; fgets(name, 80, stdin) != NULL; i = next) {
+    next = create(name);
+    if (start == NULL)
+      start = next;
+    if (i != NULL)
+      i->next = next;
+  }
 
-  isla_nublar.next = &skull;
-  skull.next = &shutter;
-
-  display(&amity);
+  display(start);
+  release(start);
 }
